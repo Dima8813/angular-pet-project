90994c07cd2536dd6eb4c1849674792c999d650f
Dmytro <dmytro.kudriavtsev88@gmail.com>
2023-10-26 11:43:42 +0200

    [setup project]: add husky

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'login'},
+  {path: 'login', loadComponent: () => import('@pages/auth/components/login/login.component').then(m => m.LoginComponent)},
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
